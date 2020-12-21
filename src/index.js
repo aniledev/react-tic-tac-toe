750b2ea5b4964ff7547dfe1d5ef05f48b8950d69
aniledev <elinamcgill@gmail.com>
2020-12-21 18:13:22 -0500

    feat: use this.state to allow X and O to take turns when click on square

--- src/index.js
+++ src/index.js
@@ -1,33 +1,53 @@
+// To collect data from multiple children, or to have two child components communicate with each other, you need to declare the shared state in their parent component instead.
+// The parent component can pass the state back down to the children by using props; this keeps the child components in sync with each other and with the parent component.
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  // React components can have state by setting this.state in their constructors
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      <button
-        className="square"
-        // adding an onClick callback to the square to make sure they are interactive
-        // when calling setState, React automaticaly updates the child components inside of it too
-        onClick={() => this.setState({ value: "X" })}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  // rewrite Square as a function component, not a class component, because it no longer tracks its own state
+  return (
+    <button
+      className="square"
+      // adding an onClick callback to the square to make sure they are interactive
+      // when calling setState, React automaticaly updates the child components inside of it too
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    // the below line creates an this.state object with a key of squares and property which is an array which is a length of 9, with all values being null
+    this.state = {
+      squares: Array(9).fill(null),
+      // each time a player moves, xIsNext will be flipped to determine which player goes next and the game's state will be saved
+      xIsNext: true,
+    };
+  }
+
+  // this changes the squares array the state of each square is being kept
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    //this allows X and O to take turns when a square is clicked on
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
