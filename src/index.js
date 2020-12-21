165fcdf52fdb1df40d1ac9df7975e65b98ac90e2
aniledev <elinamcgill@gmail.com>
2020-12-21 18:54:19 -0500

    chore: add a calculateWinner() that provides winning arrays

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <div>
@@ -90,6 +90,28 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  // this holds the arrays of winning lines in the tic tac toe game
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // using a loop to compare the individual square state to the winning lines array
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 ReactDOM.render(<Game />, document.getElementById("root"));
