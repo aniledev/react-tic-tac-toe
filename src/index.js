e8da52b100b14c29a4531252ecab9c2228f69ff2
aniledev <elinamcgill@gmail.com>
2020-12-23 03:47:45 -0500

    style: update board color and font sizes

--- src/index.js
+++ src/index.js
@@ -108,13 +108,13 @@ class Game extends React.Component {
 
     return (
       <div className="game">
-        <div className="game-board">
+        <div className="game-board flex">
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
-        <div className="game-info">
+        <div className="game-info flex">
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
